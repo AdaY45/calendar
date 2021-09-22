7762baa1f37ce35f2d4097edcd072082585198ad
AdaY45 <afsdert2012@gmail.com>
2021-09-22 19:04:38 +0300

    change logo image extension

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import logo from "../public/logo.png";
+import logo from "../public/logoo.png";
 import styles from "../styles/Header.module.scss";
 
 const Header = (props) => {
